b905a5e73450c5594db91575bd0a9c704996d1fa
Liviadbelao <125027552+Liviadbelao@users.noreply.github.com>
2023-08-18 10:17:52 -0300

    feat: create classes

---
+++ assets/js/script.category-product.js
@@ -0,0 +1,35 @@
+//categorias que possuem vários produtos 
+//cada produto é pertencente a uma categoria 
+
+class Category{
+    constructor(id, name){
+      this.id = id;
+      this.name = name;
+      this.products = [];
+    }
+}
+class Product{
+    constructor(id, name, price, category){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
+class CategoryService{
+    constructor(){
+        this.categories = [];
+        this.nextCategoryId = 1;
+    }
+
+    addCategory(name){
+     const id = this.nextCategoryId;
+     this.nextCategoryId++;
+
+     const category = new Category(id, name);
+     this.categories.push(category);
+    }
+}
+const categoriesList = new CategoryService();
+console.log(categoriesList)
+function createCategories(){}
